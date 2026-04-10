2f745784662d86319febce2694e1f272ca06fd46
yasua006 <yasua006@osloskolen.no>
2026-04-10 12:58:58 +0200

    feat(init.lua): lag html, css, og json servere og litt mer lesbart configs og enables
    Lagde HTML, CSS, og JSON servere
    Gjorte lsp configs og lsp enables til funksjoner

--- init.lua
+++ init.lua
@@ -42,6 +42,7 @@ vim.opt.showmatch = true
 -- default: 20
 vim.opt.history = 100
 
+
 local function plug_installs()
     vim.cmd([[
         call plug#begin("~/.config/nvim/lua/plugins")
@@ -70,7 +71,7 @@ plug_installs()
 
 local coq = require("coq")
 
-vim.lsp.config["lua-ls"] = {
+vim.lsp.config["lua_ls"] = {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
     settings = {
@@ -82,11 +83,34 @@ vim.lsp.config["lua-ls"] = {
     }
 }
 
-coq.lsp_ensure_capabilities()
+local function lsp_configs()
+    -- HTML
+    vim.lsp.config["html_server"] = {
+        cmd = { 'vscode-html-language-server', '--stdio' }
+    }
+    vim.lsp.config["css_server"] = {
+        cmd = { 'vscode-css-language-server', '--stdio' }
+    }
+
+    -- JSON
+    vim.lsp.config["json_server"] = {
+        cmd = { 'vscode-json-language-server', '--stdio' }
+    }
+end
+
+lsp_configs()
+
 coq.lsp_ensure_capabilities()
 
-vim.lsp.enable("lua-ls")
-vim.lsp.enable("pyright")
+local function lsp_enables()
+    vim.lsp.enable("lua_ls")
+    vim.lsp.enable("pyright")
+    vim.lsp.enable("html_server")
+    vim.lsp.enable("css_server")
+    vim.lsp.enable("json_server")
+end
+
+lsp_enables()
 
 local tree_config = {
     view = {
