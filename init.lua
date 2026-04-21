cba415ec08e34d05c7fd0916079c3c2f8cb4ad3b
yasua006 <yasua006@osloskolen.no>
2026-04-21 19:03:25 +0200

    fix(Comment nvim fungerer ikke som den skal. #2): Feil rett fil kommentering
    Brukte pre_hook til "Comment.nvim" for å rette kommentering utenfor konfig
    mappen
    
    Andre
    Legget til js, ts, c, cpp, og cs for multi linjer og kommentering til shell filene

--- init.lua
+++ init.lua
@@ -123,7 +123,42 @@ local tree_config = {
 require("lualine").setup()
 require("nvim-tree").setup(tree_config)
 require("prettier").setup()
-require("Comment").setup()
+
+common_multi_line_comment = "/* %s */"
+
+local function same_comments()
+    if vim.bo.filetype == "html" then
+        return "<!-- %s -->"
+    end
+    if vim.bo.filetype == "css" then
+        return common_multi_line_comment
+    end
+end
+
+require("Comment").setup({
+    pre_hook = function()
+        same_comments()
+
+        if (vim.bo.filetype == "javascript" or
+            vim.bo.filetype == "typescript") or
+            (vim.bo.filetype == "c" or
+            vim.bo.filetype == "cpp") or
+            vim.bo.filetype == "cs" then
+            return common_multi_line_comment
+	    end
+
+        if vim.bo.filetype == "python" then
+            return ' """ %s """ '
+        end
+
+        if (vim.bo.filetype == "sh" or
+            vim.bo.filetype == "zsh") or
+            vim.bo.filetype == "bash" then
+            return "# %s"
+        end
+    end
+})
+
 require("catppuccin").setup({
     flavour = "auto", -- latte, frappe, macchiato, mocha
     background = { -- :h background
